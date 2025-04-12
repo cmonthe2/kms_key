776481828117e84acb472d162105a1ab8ff6f4ea
cmonthe2 <88361315+cmonthe2@users.noreply.github.com>
2025-04-12 13:16:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,20 @@ python3 kms-import-script.py --region us-west-2
 
 LIST OF KEYS 
 aws kms list-keys --region us-east-1
+
+
+[profile d2c-non-prod]
+sso_session = rb
+sso_account_id = 12334444444
+sso_role_name = AWS-SSO-me
+region = us-west-2
+output = json
+sso_region = us-west-2
+
+[sso-session eig]
+ 
+sso_start_url = https://d-ffr5555.awsapps.com/start/#
+ 
+sso_region = us-west-2
+ 
+sso_registration_scopes = sso:account:access
