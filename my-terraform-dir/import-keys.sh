6df61ccd4d21ffba48303b27da887400205c5750
cmonthe2 <88361315+cmonthe2@users.noreply.github.com>
2025-04-23 15:02:17 -0400

    Update import-keys.sh

--- my-terraform-dir/import-keys.sh
+++ my-terraform-dir/import-keys.sh
@@ -78,42 +78,44 @@ resource "aws_kms_key_policy" "monthly_key_policy" {
 
 resource "aws_kms_key_policy" "AWSBackupServiceKey" {
   key_id = aws_kms_key.AWSBackupServiceKey.key_id
+
   policy = jsonencode({
-    "Version": "2012-10-17",
-  "Id": "key-consolepolicy-3",
-  "Statement": [
-    {
-      "Sid": "Enable IAM User Permissions",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::73783999:root"
+    Version = "2012-10-17",
+    Id      = "key-consolepolicy-3",
+    Statement = [
+      {
+        Sid      = "Enable IAM User Permissions",
+        Effect   = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::73783999:root"
+        },
+        Action   = "kms:*",
+        Resource = "*"
       },
-      "Action": "kms:*",
-      "Resource": "*"
-    },
-    {
-      "Sid": "Allow access for Key Administrators",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::7378388:role/aws-reserved/sso.amazonaws.com/us-west-2/AWSReservedSSO_AdministratorAccess_b99ad3d2e12b5b7c"
-      },
-      "Action": [
-        "kms:Create*",
-        "kms:Describe*",
-        "kms:Enable*",
-        "kms:List*",
-        "kms:Put*",
-        "kms:Update*",
-        "kms:Revoke*",
-        "kms:Disable*",
-        "kms:Get*",
-        "kms:Delete*",
-        "kms:TagResource",
-        "kms:UntagResource",
-        "kms:ScheduleKeyDeletion",
-        "kms:CancelKeyDeletion"
-      ],
-      "Resource": "*"
-    }
-  ]
-})
+      {
+        Sid      = "Allow access for Key Administrators",
+        Effect   = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::7378388:role/aws-reserved/sso.amazonaws.com/us-west-2/AWSReservedSSO_AdministratorAccess_b99ad3d2e12b5b7c"
+        },
+        Action = [
+          "kms:Create*",
+          "kms:Describe*",
+          "kms:Enable*",
+          "kms:List*",
+          "kms:Put*",
+          "kms:Update*",
+          "kms:Revoke*",
+          "kms:Disable*",
+          "kms:Get*",
+          "kms:Delete*",
+          "kms:TagResource",
+          "kms:UntagResource",
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
